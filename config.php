6e9fa5cd70b644dc623d0592e7771bdd1dfe327f
Fritz <fjbeimesch@fuse.net>
2018-12-18 21:42:26 -0500

    add config.php skeleton

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config= [
+	'bot_id' => ''
+];
+$messages= [
+	'weekly_stats' => ''
+];
+// If a player has a space in the name be sure to use %20 instead to follow url encoding standard
+$players= [
+	''
+];
